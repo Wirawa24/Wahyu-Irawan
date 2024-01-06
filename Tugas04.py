4069b3bfd109e45e70a820942c20f88dd60d0c3e
Wahyu Irawan <147359187+Wirawa24@users.noreply.github.com>
2024-01-06 09:44:22 +0700

    Add files via upload
    Tugas 04 Studi kasus permasalahan sederhana

---
+++ Tugas04.py
@@ -0,0 +1,22 @@
+
+def input["a","b","c",("double d"),"i","X1","X2"]
+D = (b*b)-(4*a*c)
+D = 0 ;
+def print(f'Akar-akar X1)=('X2 real kembar);
+X1 = -b/(2*a)
+X2 = X1
+print(X1,X2)
+End;
+if D > 0 ;
+def print(f"Akar-akar X1 dan X2 berlainan");
+X1 = (-b + sqrt (d))/(2*a)
+X2 = (-b- sqrt (d))/(2*a)
+print (X1,X2)
+End;
+if D < 0 ;
+def print(f"Akar-akar X1 dan X2 imajiner berlainan");
+i = sqrt(-d)/(2*a)
+X1 = (-b/(2*a))+ (sqrt(-d))/(2*a)*i
+X2 = (-b/(2*a))+ (sqrt(-d))/(2*a)*i
+print(X1,X2)
+End;
